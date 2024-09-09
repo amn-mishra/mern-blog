7008160793f2e4fc859aa863dede15105f0747fd
amanmishra131 <aman11nov2021@gmail.com>
2024-09-10 00:37:39 +0530

    Create a test API Route

--- api/index.js
+++ api/index.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv' ; 
+import userRoutes from './routes/user.route.js' ;
 
+// this is for to use .env file where confidential details are present
 dotenv.config();
 
+
+// we are connect with database 
 mongoose.connect(process.env.MONGO).then( () => { 
     console.log('Mongo is connected')
 }).catch(err => {
     console.log(err)
 });
 
-
 const app = express()
 
-
-
 app.listen(3000, () =>{
-
     console.log('Server is running on port 3000 ok ')
-
 });
+
+app.use('/api/user', userRoutes );
